a55b5665cc2927e6abbab941b2d47733c86cf61b
Ian Fisher <ian@iafisher.com>
2024-06-03 10:31:55 -0400

    rpi-os: boot into exception level 1

---
+++ 2024/q2/rc/rpi-os/include/arm/sysregs.h
@@ -0,0 +1,53 @@
+#ifndef SYSREGS_H
+#define SYSREGS_H
+
+// ***************************************
+// SCTLR_EL1, System Control Register (EL1), Page 2654 of AArch64-Reference-Manual.
+// ***************************************
+
+// set reserved bits to 1
+#define SCTLR_RESERVED                  (3 << 28) | (3 << 22) | (1 << 20) | (1 << 11)
+
+// make the processor little-endian at EL1
+#define SCTLR_EE_LITTLE_ENDIAN          (0 << 25)
+
+// make the processor little-endian at EL0
+#define SCTLR_EOE_LITTLE_ENDIAN         (0 << 24)
+
+// disable instruction and data caches for simplicity
+#define SCTLR_I_CACHE_DISABLED          (0 << 12)
+#define SCTLR_D_CACHE_DISABLED          (0 << 2)
+
+// enable/disable the MMU
+#define SCTLR_MMU_DISABLED              (0 << 0)
+#define SCTLR_MMU_ENABLED               (1 << 0)
+
+#define SCTLR_VALUE_MMU_DISABLED	(SCTLR_RESERVED | SCTLR_EE_LITTLE_ENDIAN | SCTLR_I_CACHE_DISABLED | SCTLR_D_CACHE_DISABLED | SCTLR_MMU_DISABLED)
+
+// ***************************************
+// HCR_EL2, Hypervisor Configuration Register (EL2), Page 2487 of AArch64-Reference-Manual.
+// ***************************************
+
+#define HCR_RW	    			(1 << 31)
+#define HCR_VALUE			HCR_RW
+
+// ***************************************
+// SCR_EL3, Secure Configuration Register (EL3), Page 2648 of AArch64-Reference-Manual.
+// ***************************************
+
+#define SCR_RESERVED	    		(3 << 4)
+#define SCR_RW				(1 << 10)
+#define SCR_NS				(1 << 0)
+#define SCR_VALUE	    	    	(SCR_RESERVED | SCR_RW | SCR_NS)
+
+// ***************************************
+// SPSR_EL3, Saved Program Status Register (EL3) Page 389 of AArch64-Reference-Manual.
+// ***************************************
+
+// disable all interrupts
+#define SPSR_MASK_ALL 			(7 << 6)
+// use dedicated EL1 stack pointer
+#define SPSR_EL1h			(5 << 0)
+#define SPSR_VALUE			(SPSR_MASK_ALL | SPSR_EL1h)
+
+#endif
