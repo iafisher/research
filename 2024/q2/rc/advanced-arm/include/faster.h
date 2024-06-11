197cd532ab8786c2d959b2c107f94c39fb29cdef
Ian Fisher <ian@iafisher.com>
2024-06-11 15:49:20 -0400

    advanced-arm: check for overflow

--- 2024/q2/rc/advanced-arm/include/faster.h
+++ 2024/q2/rc/advanced-arm/include/faster.h
@@ -1,9 +1,21 @@
 #ifndef IAN_FASTER_H
 #define IAN_FASTER_H
 
+#include <stdint.h>
+
+typedef uint64_t u64;
+
 // The `asm` directive instructs Clang not to prefix the symbol name with an underscore.
 int ian_add(int, int) asm("ian_add");
 int ian_strlen(const char*) asm("ian_strlen");
 void ian_rot13(char*) asm("ian_rot13");
+void ian_overflow(void) asm("ian_overflow");
+
+struct u64_or_overflow {
+  u64 r;
+  int v;
+};
+
+struct u64_or_overflow ian_add_check_overflow(u64, u64) asm("ian_add_check_overflow");
 
 #endif /* IAN_FASTER_H */
--- 2024/q2/rc/advanced-arm/src/main.c
+++ 2024/q2/rc/advanced-arm/src/main.c
@@ -2,6 +2,8 @@
 
 #include "faster.h"
 
+void add_check_overflow_print(u64, u64);
+
 int main(int argc, char* argv[]) {
   int result = ian_add(20, 22);
   printf("ian_add result: %d\n", result);
@@ -13,5 +15,19 @@ int main(int argc, char* argv[]) {
   ian_rot13(s);
   printf("ian_rot13 result: %s\n", s);
 
+  /* ian_overflow(); */
+
+  add_check_overflow_print(20, 22);
+  add_check_overflow_print(0xffffffffffffffff, 1);
+
   return 0;
+}
+
+void add_check_overflow_print(u64 x, u64 y) {
+  struct u64_or_overflow r = ian_add_check_overflow(x, y);
+  if (r.v) {
+    printf("%llu + %llu = overflow!\n", x, y);
+  } else {
+    printf("%llu + %llu = %llu\n", x, y, r.r);
+  }
 }
