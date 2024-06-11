7d8d2e724d44117f507ee3d1ff2f116bdc5bbe89
Ian Fisher <ian@iafisher.com>
2024-06-11 14:30:57 -0400

    advanced-arm: rot13

--- 2024/q2/rc/advanced-arm/include/faster.h
+++ 2024/q2/rc/advanced-arm/include/faster.h
@@ -4,5 +4,6 @@
 // The `asm` directive instructs Clang not to prefix the symbol name with an underscore.
 int ian_add(int, int) asm("ian_add");
 int ian_strlen(const char*) asm("ian_strlen");
+void ian_rot13(char*) asm("ian_rot13");
 
 #endif /* IAN_FASTER_H */
--- 2024/q2/rc/advanced-arm/src/main.c
+++ 2024/q2/rc/advanced-arm/src/main.c
@@ -6,9 +6,12 @@ int main(int argc, char* argv[]) {
   int result = ian_add(20, 22);
   printf("ian_add result: %d\n", result);
 
-  const char* s = "hello";
+  char s[] = "hello";
   result = ian_strlen(s);
   printf("ian_strlen result: %d\n", result);
+
+  ian_rot13(s);
+  printf("ian_rot13 result: %s\n", s);
 
   return 0;
 }
