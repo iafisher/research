af33188a912c2be11ccf7935152938548e0cf4cd
Ian Fisher <ian@iafisher.com>
2024-07-17 14:43:46 -0400

    browser-engineering: lay out text char by char

--- 2024/q2/rc/browser-engineering/internal/gui.go
+++ 2024/q2/rc/browser-engineering/internal/gui.go
@@ -1,38 +1,85 @@
 package internal
 
 import (
+	"fmt"
+	"os"
+	"strings"
+
 	"github.com/veandco/go-sdl2/sdl"
+	"github.com/veandco/go-sdl2/ttf"
 )
 
 type Gui struct {
 	Width  int32
 	Height int32
 	window *sdl.Window
+	font   *ttf.Font
 }
 
 func (gui *Gui) Init() error {
-	err := sdl.Init(sdl.INIT_EVERYTHING)
+	err := ttf.Init()
 	if err != nil {
 		return err
 	}
-	gui.window, err = sdl.CreateWindow("test", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, gui.Width, gui.Height, sdl.WINDOW_SHOWN)
+
+	err = sdl.Init(sdl.INIT_EVERYTHING)
+	if err != nil {
+		return err
+	}
+
+	// gui.font, err = ttf.OpenFont("./assets/Atkinson_Hyperlegible/AtkinsonHyperlegible-Regular.ttf", 16)
+	gui.font, err = ttf.OpenFont("/System/Library/Fonts/Supplemental/Arial Unicode.ttf", 16)
+	if err != nil {
+		return err
+	}
+
+	gui.window, err = sdl.CreateWindow("TinCan", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, gui.Width, gui.Height, sdl.WINDOW_SHOWN)
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func (gui *Gui) Show() error {
+func (gui *Gui) ShowTextPage(text string) error {
 	surface, err := gui.window.GetSurface()
 	if err != nil {
 		return err
 	}
-	surface.FillRect(nil, 0)
 
-	rect := sdl.Rect{0, 0, 200, 200}
-	color := sdl.Color{R: 255, G: 0, B: 255, A: 255}
-	pixel := sdl.MapRGBA(surface.Format, color.R, color.G, color.B, color.A)
-	surface.FillRect(&rect, pixel)
+	text = stripNewlines(text)
+	var cursorX int32 = 0
+	var cursorY int32 = 0
+	for _, c := range text {
+		// TODO: why does this URL have so many null bytes?
+		// https://browser.engineering/examples/xiyouji.html
+		if c == 0 {
+			continue
+		}
+
+		renderedText, err := gui.font.RenderUTF8Blended(string(c), sdl.Color{R: 255, G: 255, B: 255, A: 255})
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "gui: warning: could not render character (code=%d): %s\n", c, err.Error())
+			continue
+		}
+		defer renderedText.Free()
+
+		err = renderedText.Blit(nil, surface, &sdl.Rect{X: cursorX, Y: cursorY})
+		if err != nil {
+			return err
+		}
+
+		cursorX += 15
+		if cursorX >= gui.Width {
+			cursorX = 0
+			cursorY += 18
+
+			if cursorY >= gui.Height {
+				fmt.Fprintln(os.Stderr, "gui: warning: fell off bottom of page")
+				break
+			}
+		}
+	}
+
 	gui.window.UpdateSurface()
 
 	running := true
@@ -50,7 +97,13 @@ func (gui *Gui) Show() error {
 	return nil
 }
 
+func stripNewlines(text string) string {
+	return strings.ReplaceAll(text, "\n", " ")
+}
+
 func (gui *Gui) Cleanup() {
-	sdl.Quit()
 	gui.window.Destroy()
+	gui.font.Close()
+	sdl.Quit()
+	ttf.Quit()
 }
--- 2024/q2/rc/browser-engineering/main.go
+++ 2024/q2/rc/browser-engineering/main.go
@@ -1,55 +1,59 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 
 	"github.com/iafisher/browser-engineering/internal"
 )
 
 func main() {
+	verbose := flag.Bool("verbose", false, "turn on verbose output")
+	noGui := flag.Bool("no-gui", false, "do not open browser GUI")
+	flag.Parse()
+
+	if *verbose {
+		internal.SetVerbose(true)
+	}
+
+	argCount := len(flag.Args())
+	if argCount == 0 {
+		fmt.Fprintf(os.Stderr, "error: one command-line argument required\n")
+		os.Exit(1)
+	}
+
+	fetcher := internal.NewUrlFetcher()
+	defer fetcher.Cleanup()
+
 	gui := internal.Gui{Width: 800, Height: 600}
-	gui.Init()
-	defer gui.Cleanup()
-	gui.Show()
-	// verbose := flag.Bool("verbose", false, "turn on verbose output")
-	// noPrint := flag.Bool("no-print", false, "do not print responses")
-	// flag.Parse()
+	if !*noGui {
+		gui.Init()
+		defer gui.Cleanup()
+	}
 
-	// if *verbose {
-	// 	internal.SetVerbose(true)
-	// }
+	success := true
+	for _, urlString := range flag.Args() {
+		if argCount > 1 {
+			fmt.Printf("tincan: fetching URL %s\n\n", urlString)
+		}
+		err := fetchAndShowOne(&fetcher, &gui, urlString, *noGui)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error: could not fetch URL %s: %s\n", urlString, err.Error())
+			success = false
+		} else {
+			if argCount > 1 {
+				fmt.Printf("tincan: finished fetching URL %s\n\n", urlString)
+			}
+		}
+	}
 
-	// argCount := len(flag.Args())
-	// if argCount == 0 {
-	// 	fmt.Fprintf(os.Stderr, "error: one command-line argument required\n")
-	// 	os.Exit(1)
-	// }
-
-	// fetcher := internal.NewUrlFetcher()
-	// defer fetcher.Cleanup()
-
-	// success := true
-	// for _, urlString := range flag.Args() {
-	// 	if argCount > 1 {
-	// 		fmt.Printf("tincan: fetching URL %s\n\n", urlString)
-	// 	}
-	// 	err := fetchAndPrintOne(&fetcher, urlString, *noPrint)
-	// 	if err != nil {
-	// 		fmt.Fprintf(os.Stderr, "error: could not fetch URL %s: %s\n", urlString, err.Error())
-	// 		success = false
-	// 	} else {
-	// 		if argCount > 1 {
-	// 			fmt.Printf("tincan: finished fetching URL %s\n\n", urlString)
-	// 		}
-	// 	}
-	// }
-
-	// if !success {
-	// 	os.Exit(2)
-	// }
+	if !success {
+		os.Exit(2)
+	}
 }
 
-func fetchAndPrintOne(fetcher *internal.UrlFetcher, urlString string, noPrint bool) error {
+func fetchAndShowOne(fetcher *internal.UrlFetcher, gui *internal.Gui, urlString string, noGui bool) error {
 	url, err := internal.ParseUrl(urlString)
 	if err != nil {
 		return err
@@ -60,11 +64,18 @@ func fetchAndPrintOne(fetcher *internal.UrlFetcher, urlString string, noPrint bo
 		return err
 	}
 
-	if !noPrint {
+	if !noGui {
+		var text string
 		if url.ViewSource {
-			fmt.Println(response.GetContent())
+			text = response.GetContent()
 		} else {
-			fmt.Println(response.GetTextContent())
+			text = response.GetTextContent()
+		}
+
+		fmt.Println(text)
+		err = gui.ShowTextPage(text)
+		if err != nil {
+			return err
 		}
 	}
 
